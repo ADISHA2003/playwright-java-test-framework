9756bde3dcd4ad7e3351000e4699730dabda404e
ADISHA2003 <adityasharma56697@gmail.com>
2026-04-21 22:11:51 +0530

    deleted db creds

--- src/main/java/Constants/MongoConstants.java
+++ src/main/java/Constants/MongoConstants.java
@@ -2,8 +2,8 @@ package Constants;
 
 public class MongoConstants {
 
-    public static final String MONGODB_URI = "mongodb+srv://vccedge:vccedge#2wert@cluster0.epsk1yv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    public static final String DEV_MONGO_URI = "mongodb+srv://appu4vcidevdb:o3lE5Xpdo6KDV5GW@vci-dev.dyp9k.mongodb.net/?retryWrites=true&w=majority&appName=VCI-Dev";
-    public static final String PROD_MONGO_URI = "mongodb+srv://prod-nishanshu-211521:VHwaONQXkDeHhVWz@vci-prod.y5dof.mongodb.net/?retryWrites=true&w=majority&appName=vci-prod&readPreference=secondary";
+    public static final String MONGODB_URI = System.getenv("MONGODB_URI");
+    public static final String DEV_MONGO_URI = System.getenv("DEV_MONGO_URI");
+    public static final String PROD_MONGO_URI = System.getenv("PROD_MONGO_URI");
     public static final String DATABASE_NAME = "vci";
 }
